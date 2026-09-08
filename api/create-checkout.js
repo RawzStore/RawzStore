673457699efeef9498fa9747ad0ea1a2f2dc77ac
Thomas <totoriou09@gmail.com>
2026-09-08 20:00:52 +0200

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -7,7 +7,8 @@ const productsCatalog = [
   { id: 2, name: "Polo Rayures", price: 1799 },
   { id: 3, name: "Polo Baggy Court", price: 1799 },
   { id: 4, name: "T-Shirt Tricoté", price: 2499 },
-  { id: 5, name: "Short Double Layer", price: 2499 }
+  { id: 5, name: "Short Double Layer", price: 2499 },
+  { id: 6, name: "Jogging Baggy Grey", price: 3499 }
 ];
 
 export default async function handler(req, res) {
